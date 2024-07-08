290ba0bb0df14a8a92cadeb5ad094bd86dbf9b78
s-damian <contact@damian-freelance.fr>
2024-07-08 15:43:13 +0300

    Init tailwindcss

--- app/tickets-swap/src/App.tsx
+++ app/tickets-swap/src/App.tsx
@@ -3,13 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateEvent from "./pages/CreateEvent";
 import SwapTicket from "./pages/SwapTicket";
-import "./App.css";
 import NavBar from "./components/NavBar";
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="app">
+            <div className="container mx-auto text-center">
                 <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
--- app/tickets-swap/src/components/NavBar.tsx
+++ app/tickets-swap/src/components/NavBar.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import "./NavBar.css";
 
 const NavBar: React.FC = () => {
     return (
-        <nav className="nav-bar">
-            <ul>
+        <nav className="bg-blue-600 p-4">
+            <ul className="flex justify-around items-center list-none p-0 m-0">
                 <li>
-                    <NavLink to="/">Home</NavLink>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            `text-white no-underline p-2 ${isActive ? "border-b-2 border-white" : "hover:border-b-2 hover:border-gray-300"}`
+                        }
+                    >
+                        Home
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/create-event">Create Event</NavLink>
+                    <NavLink
+                        to="/create-event"
+                        className={({ isActive }) =>
+                            `text-white no-underline p-2 ${isActive ? "border-b-2 border-white" : "hover:border-b-2 hover:border-gray-300"}`
+                        }
+                    >
+                        Create Event
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/swap-ticket">Swap Ticket</NavLink>
+                    <NavLink
+                        to="/swap-ticket"
+                        className={({ isActive }) =>
+                            `text-white no-underline p-2 ${isActive ? "border-b-2 border-white" : "hover:border-b-2 hover:border-gray-300"}`
+                        }
+                    >
+                        Swap Ticket
+                    </NavLink>
                 </li>
-                <li className="wallet-button">
-                    <WalletMultiButton />
+                <li className="ml-auto">
+                    <WalletMultiButton className="btn btn-primary" />
                 </li>
             </ul>
         </nav>
