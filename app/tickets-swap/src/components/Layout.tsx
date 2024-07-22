5c55f981f37853bf7657acae74f488c63a6d0b70
s-damian <contact@damian-freelance.fr>
2024-07-22 12:05:11 +0300

    Optimie layout when user is not auth

--- app/tickets-swap/src/components/Layout.tsx
+++ app/tickets-swap/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
 import { ToastContainer, toast } from "react-toastify";
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import "react-toastify/dist/ReactToastify.css";
 
 interface LayoutProps {
@@ -8,6 +9,8 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+    const wallet = useAnchorWallet();
+
     const contextClass = {
         success: "bg-blue-600",
         error: "bg-red-600",
@@ -21,21 +24,29 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <div className="container mx-auto text-center">
             <NavBar />
             <div className="min-h-screen bg-gray-100 flex flex-col py-12 px-8">
-                {children}
-                <ToastContainer
-                    position="top-right"
-                    autoClose={5000}
-                    hideProgressBar={true}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    toastClassName={(context) => contextClass[context?.type || "default"] + " overflow-hidden"}
-                    bodyClassName={() => "text-sm font-white font-med block p-3"}
-                    className="toast-container"
-                />
+                {wallet?.publicKey ? (
+                    <>
+                        {children}
+                        <ToastContainer
+                            position="top-right"
+                            autoClose={5000}
+                            hideProgressBar={true}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                            toastClassName={(context) => contextClass[context?.type || "default"] + " overflow-hidden"}
+                            bodyClassName={() => "text-sm font-white font-med block p-3"}
+                            className="toast-container"
+                        />
+                    </>
+                ) : (
+                    <div className="flex flex-col items-center justify-center h-full">
+                        <p className="text-xl text-gray-800 mb-4">Veuillez connecter votre portefeuille pour continuer.</p>
+                    </div>
+                )}
             </div>
         </div>
     );
